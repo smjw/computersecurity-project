243c01a424b7a87c585179fba563605f92e08fed
Sophia Williams <117466337+smjw@users.noreply.github.com>
2024-11-24 19:39:45 +0000

    restart

--- project/compSecProj/customers/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views  # Import the views from the users app
-
-urlpatterns = [
-    path('register/', views.register, name='register'),  # Register URL mapped to the 'register' view
-]
