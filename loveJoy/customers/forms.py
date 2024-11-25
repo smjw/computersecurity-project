0f5905e283c77c1f96a126f70cf09b921d23ab6d
Sophia Williams <117466337+smjw@users.noreply.github.com>
2024-11-25 23:42:40 +0000

    ??

--- loveJoy/customers/forms.py
+++ loveJoy/customers/forms.py
@@ -3,6 +3,10 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import Customer
 
 class CustomerRegistrationForm(UserCreationForm):
+    security_answer = forms.CharField(
+        label="Security Question: What is your mother's maiden name?",
+        widget=forms.TextInput(attrs={'placeholder':'enter answer'}),
+    )
     class Meta:
         model = Customer
-        fields = ["username", "email", "first_name", "last_name", "phone_number", "password1", "password2"]
+        fields = ["username", "email", "first_name", "last_name", "phone_number", "password1", "password2", "security_answer"]
--- loveJoy/customers/models.py
+++ loveJoy/customers/models.py
@@ -9,9 +9,10 @@ class Customer(AbstractUser):
     first_name = models.CharField(max_length=50, null=False, blank=False)
     last_name = models.CharField(max_length=50, null=False, blank=False)
     phone_number = PhoneNumberField(unique=True, null=False, blank=False)
+    security_answer = models.CharField(max_length=255, null=False, blank=False)
 
 
-    REQUIRED_FIELDS = ("email", "first_name", "last_name", "phone_number")
+    REQUIRED_FIELDS = ("email", "first_name", "last_name", "phone_number", "security_answer")
 
     def __str__(self):
         return self.username
